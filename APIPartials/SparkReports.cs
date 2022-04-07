95fe80fd83dab60b66dc535de4e8d163334f30e8
aebim <michael.aebi@sunrise.net>
2022-04-07 10:45:37 +0200

    * TargetFramework changed to net4.8 SparkReports.cs Using cleanup

--- APIPartials/SparkReports.cs
+++ APIPartials/SparkReports.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace SparkDotNet
 {

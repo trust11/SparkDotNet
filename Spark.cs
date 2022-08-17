025b0c3215377f099fa8bc1cdf3d07afcb3dd968
Stephan Steiner <stephan.steiner@gmail.com>
2022-08-17 22:48:05 +0200

    implemented dummy Login/Logout/Token refresh added Configuration object returned static helper property to get a success result

--- Spark.cs
+++ Spark.cs
@@ -14,6 +14,7 @@ using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using static SparkDotNet.ExceptionHandling.SparkApiOperationResultMapper;
 using static System.Net.WebUtility;
@@ -24,14 +25,31 @@ namespace SparkDotNet
     {
         private const string baseURL = "https://webexapis.com";
 
-        private HttpClient client = new HttpClient();
+        private HttpClient client;
+
+        private HttpClientHandler clientHandler;
+
+        public string HostName => baseURL;
+
+        private readonly SemaphoreSlim tokenRefreshLock = new SemaphoreSlim(1, 1);
 
         public static TicketInformations TicketInformations { get; private set; } = new TicketInformations();
 
-        private string accessToken { get; set; }
+        public string AccessToken { get; private set; }
+
+        public string Id => config?.Id;
+
+        private readonly SparkConfiguration config;
 
         public Spark(string accessToken)
+            : this(new SparkConfiguration { Token = accessToken })
         {
+        }
+
+        public Spark(SparkConfiguration config)
+        {
+            this.config = config;
+            SetupHttpClient();
             var serializerSettings = new JsonSerializerSettings
             {
                 NullValueHandling = NullValueHandling.Include,
@@ -40,11 +58,187 @@ namespace SparkDotNet
             serializerSettings.Converters.Add(new StringEnumConverter(true));
             serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); // use camel case so we can use proper .net notation
             JsonConvert.DefaultSettings = () => serializerSettings;
+        }
 
-            this.accessToken = accessToken;
+        private void SetupHttpClient()
+        {
+            clientHandler = new HttpClientHandler
+            {
+                //ServerCertificateCustomValidationCallback = valdiateCert
+            };
+            if (config.NoProxy)
+            {
+                Log($"Proxy for {HostName} has been disabled by configuration", 5);
+                clientHandler.UseProxy = false;
+            }
+            else if (clientHandler.UseProxy)
+            {
+                if (clientHandler.Proxy != null)
+                {
+                    var proxy = clientHandler.Proxy.GetProxy(new Uri($"https://{HostName}"));
+                    Log($"Using proxy {proxy?.AbsolutePath} for {HostName}", 5);
+                }
+                else
+                    Log($"UseProxy is enabled on clientHandler for {HostName}, using system proxy", 5);
+            }
+            else
+                Log($"Not using a proxy for API requests", 5);
+            client = new HttpClient(clientHandler);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.accessToken);
+            if (!string.IsNullOrEmpty(config.Token))
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", config.Token);
+        }
+
+        public async Task<SparkApiConnectorApiOperationResult> Login(string login = null, string password = null, string applicationId = null, string secretKey = null, int timeout = 0)
+        {
+            var result = new SparkApiConnectorApiOperationResult();
+            //HttpResponseMessage response = null;
+            //try
+            //{
+            //    var auth = Base64Encode($"{login ?? config.Login}:{password ?? config.Password}");
+            //    var authString = $"{secretKey ?? config.SecretKey}{password ?? config.Password}";
+            //    var appAuth = Base64Encode($"{applicationId ?? config.ApplicationId}:{ComputeSha256Hash(authString)}");
+
+            //    var uri = $"{RestUrl}/authentication/v1.0/login";
+            //    var msg = new HttpRequestMessage(HttpMethod.Get, uri);
+            //    msg.Headers.Add("x-rainbow-app-auth", $"Basic {appAuth}");
+            //    msg.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", auth);
+            //    msg.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //    if (timeout > 0)
+            //    {
+            //        CancellationTokenSource src = new CancellationTokenSource(timeout * 1000);
+            //        response = await httpClient.SendAsync(msg, src.Token).ConfigureAwait(false);
+            //    }
+            //    else
+            //        response = await httpClient.SendAsync(msg).ConfigureAwait(false);
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            //        result.Result = JsonConvert.DeserializeObject<LoginData>(content, serializerSettings);
+            //        SetBearerToken(result.Result.Token);
+            //        Log($"successfully logged into Rainbow server using login {login ?? config.Login}", 4);
+            //        result.ResultCode = RainbowRestApiResultCode.OK;
+            //    }
+            //    else
+            //    {
+            //        result.ResultCode = await ProcessNon200HttpResponse(result, response).ConfigureAwait(false);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    ProcessException(result, e, response);
+            //}
+            result.ResultCode = SparkApiOperationResultCode.OK;
+            return result;
+        }
+
+        public async Task<SparkApiConnectorApiOperationResult> Reinitialize(SparkConfiguration newConfig = null)
+        {
+            if (AccessToken != null)
+            {
+                var credentialsChanged = config == null
+                    || (!string.IsNullOrEmpty(newConfig?.Token) && config.Token != newConfig.Token);
+                //var credentialsChanged = config == null
+                //    || (!string.IsNullOrEmpty(newConfig?.Login) && config.Login != newConfig.Login)
+                //    || (!string.IsNullOrEmpty(newConfig?.Password) && config.Password != newConfig.Password)
+                //    || (!string.IsNullOrEmpty(newConfig?.ApplicationId) && config.ApplicationId != newConfig.ApplicationId)
+                //    || (!string.IsNullOrEmpty(newConfig?.SecretKey) && config.SecretKey != newConfig.SecretKey);
+                UpdateConfiguration(newConfig);
+                if (credentialsChanged) // need to relogin anyway
+                {
+                    var loginRes = await Login(newConfig?.Login, newConfig?.Password, newConfig?.ApplicationId, newConfig?.SecretKey).ConfigureAwait(false);
+                    if (loginRes.IsSuccess)
+                        UpdateCredentials(newConfig);
+                    return loginRes;
+                }
+                else if (IsTokenRefreshRequired())
+                {
+                    return await RefreshTokenOrReLogin().ConfigureAwait(false);
+                }
+                return SparkApiConnectorApiOperationResult.Success;
+            }
+            else
+            {
+                var loginRes = await Login(newConfig?.Login, newConfig?.Password, newConfig?.ApplicationId, newConfig?.SecretKey).ConfigureAwait(false);
+                if (loginRes.IsSuccess)
+                    UpdateCredentials(newConfig);
+                return loginRes;
+            }
+        }
+
+        private void UpdateConfiguration(SparkConfiguration newConfig)
+        {
+            if (newConfig != null)
+            {
+                config.LogAction = newConfig.LogAction;
+                config.NoProxy = newConfig.NoProxy;
+            }
+        }
+
+        private void UpdateCredentials(SparkConfiguration newConfig)
+        {
+            if (newConfig != null)
+            {
+                config.Login = newConfig.Login;
+                config.Password = newConfig.Password;
+                config.ApplicationId = newConfig.ApplicationId;
+                config.SecretKey = newConfig.SecretKey;
+            }
+        }
+
+        private bool IsTokenRefreshRequired()
+        {
+            //if (parsedToken != null)
+            //{
+            //    var doRefresh = parsedToken.ValidTo < DateTime.UtcNow;
+            //    if (!doRefresh) // token is still valid
+            //    {
+            //        var timeSinceIssue = DateTime.UtcNow.Subtract(parsedToken.IssuedAt).TotalSeconds;
+            //        var tokenLifeTime = parsedToken.ValidTo.Subtract(parsedToken.IssuedAt).TotalSeconds;
+            //        if (timeSinceIssue * 2 > tokenLifeTime)
+            //            doRefresh = true;
+            //    }
+            //    return doRefresh;
+            //}
+            return false;
+        }
+
+        private async Task<SparkApiConnectorApiOperationResult> RefreshTokenOrReLogin([CallerMemberName] string methodName = null)
+        {
+            var result = new SparkApiConnectorApiOperationResult();
+            var lockAcquired = false;
+            try
+            {
+                await tokenRefreshLock.WaitAsync().ConfigureAwait(false);
+                lockAcquired = true;
+                if (!IsTokenRefreshRequired()) // check again.. so in case if a request was queued and the refresh was done, we don't run it again
+                {
+                    Log($"Skipping token refresh triggered from {methodName} because token refresh is no longer required", 4);
+                    result.ResultCode = SparkApiOperationResultCode.OK;
+                    return result;
+                }
+                //if (rainbowToken.CountRenewed < rainbowToken.MaxTokenRenew)
+                //{
+                //    return await RenewToken().ConfigureAwait(false);
+                //}
+                //else
+                //{
+                //    Log($"Maximum number of token refreshes has been exhausted, logging in again", 4);
+                //    return await Login().ConfigureAwait(false);
+                //}
+                return await Login().ConfigureAwait(false);
+            }
+            finally
+            {
+                if (lockAcquired)
+                    tokenRefreshLock.Release();
+            }
+        }
+
+        public async Task<SparkApiConnectorApiOperationResult> Logout(int timeout = 0)
+        {
+            return SparkApiConnectorApiOperationResult.Success;
         }
 
         #region Private Helper Methods
@@ -174,6 +368,19 @@ namespace SparkDotNet
             }
             uriBuilder.Query = queryString.ToString().Trim('&');
             return uriBuilder.Uri.ToString();
+        }
+
+        private void Log(string message, int severity)
+        {
+            if (config.LogAction != null)
+                config.LogAction(message, severity);
+            else if (severity < 5)
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} {message}");
+        }
+
+        public void SetLogAction(Action<string, int> logAction)
+        {
+            config.LogAction = logAction;
         }
 
         private async Task<SparkApiConnectorApiOperationResult<T>> PostItemAsync<T>(string path, Dictionary<string, object> bodyParams)
@@ -351,7 +558,7 @@ namespace SparkDotNet
             return result;
         }
 
-        private async Task<SparkApiConnectorApiOperationResult<T>> UpdateItemAsync<T,U>(string path, U body)
+        private async Task<SparkApiConnectorApiOperationResult<T>> UpdateItemAsync<T, U>(string path, U body)
         {
             var result = new SparkApiConnectorApiOperationResult<T>();
 
---
+++ SparkConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SparkDotNet
+{
+    public class SparkConfiguration
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string ApplicationId { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public bool NoProxy { get; set; }
+
+        public string Token { get; set; }
+
+        public string Id { get; set; }
+
+        public Action<string, int> LogAction { get; set; }
+    }
+}

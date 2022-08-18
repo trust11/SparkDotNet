561b0a508b9ba4860e4df294c2099fd2888b48ef
Stephan Steiner <stephan.steiner@gmail.com>
2022-08-18 14:03:28 +0200

    moved result to top level namespace

---
+++ SparkApiConnectorResult.cs
@@ -0,0 +1,42 @@
+﻿using GenericProvisioningLib;
+using SparkDotNet.ExceptionHandling;
+
+namespace SparkDotNet
+{
+    public class SparkApiConnectorApiOperationResult<T> : SparkApiConnectorApiOperationResult, IOperationResult<T>
+    {
+        public T Result { get; set; }
+
+        public override string ToString()
+        {
+            var inhString = base.ToString();
+            return $"{Result}\n{inhString}";
+        }
+    }
+
+    public class SparkApiConnectorApiOperationResult : GenericOperationResult
+    {
+        public SparkErrorContent Error { get; set; }
+
+        public override bool IsSuccess
+        {
+            get
+            {
+                return ResultCode == SparkApiOperationResultCode.OK;
+            }
+        }
+
+        public SparkApiOperationResultCode ResultCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"IsSuccess:{IsSuccess}";
+        }
+
+        public new static SparkApiConnectorApiOperationResult Success => new SparkApiConnectorApiOperationResult 
+        {
+            IsSuccess = true,
+            ResultCode = SparkApiOperationResultCode.OK
+        };
+    }
+}

f8d78e8f7522dc7af1106f8fd91e549f8b5a533f
Stephan Steiner <stephan.steiner@gmail.com>
2022-09-21 10:46:43 +0200

    implemented single group extraction implemented group member extraction implemented all group member extraction

--- APIPartials/SparkGroups.cs
+++ APIPartials/SparkGroups.cs
@@ -53,6 +53,84 @@ namespace SparkDotNet
             return await GetItemAsync<GroupsOverview>(path);
         }
 
+        /// <summary>
+        /// Shows details for a group, by ID.
+        /// Optionally, the members may be retreived with this request.The maximum number of members returned is 500
+        /// </summary>
+        /// <param name="groupId">A unique identifier for the group.</param>
+        /// <param name="includeMembers"></param>
+        /// <returns></returns>
+        public async Task<SparkApiConnectorApiOperationResult<Group>> GetGroup(string groupId, bool? includeMembers = null)
+        {
+            var queryParams = new Dictionary<string, string>();
+            if (includeMembers.HasValue)
+                if (includeMembers != null) queryParams.Add("includeMembers", includeMembers.ToString().ToLower());
+            var path = GetURL($"{groupsBase}/{groupId}", queryParams);
+
+            return await GetItemAsync<Group>(path).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Gets the members of a group. The default maximum members returned is 500. Control parameters is available to page through the members and to control the size of the results.
+        /// </summary>
+        /// <param name="groupId">A unique identifier for the group.</param>
+        /// <param name="startIndex">The index to start for group pagination</param>
+        /// <param name="count">Non-negative Integer. Specifies the desired number of search results per page; e.g., 100. Maximum value for the count is 500</param>
+        /// <returns></returns>
+        public async Task<SparkApiConnectorApiOperationResult<Group>> GetGroupMembers(string groupId, int? startIndex = null, int? count = null)
+        {
+            var queryParams = new Dictionary<string, string>();
+            if (startIndex.HasValue)
+                queryParams.Add("startIndex", startIndex.ToString());
+            if (count.HasValue)
+                queryParams.Add("count", count.ToString());
+            var path = GetURL($"{groupsBase}/{groupId}", queryParams);
+
+            return await GetItemAsync<Group>(path).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// gets all members of a given group regardless of how many are in the group
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns></returns>
+        public async Task<SparkApiConnectorApiOperationResult<Group>> GetAllGroupMembers(string groupId, int count = 500)
+        {
+            if (count > 500)
+                count = 500;
+            else if (count < 0)
+                count = 1;
+            var queryParams = new Dictionary<string, string>();
+            var path = $"{groupsBase}/{groupId}";
+            var initialGetRes = await GetItemAsync<Group>(path).ConfigureAwait(false);
+
+            if (!initialGetRes.IsSuccess)
+                return initialGetRes;
+            var group = initialGetRes.Result;
+            int nbItems = initialGetRes.Result.MemberSize;
+            while (group.Members.Count < nbItems)
+            {
+                queryParams.Add("startIndex", group.Members.Count.ToString());
+                queryParams.Add("count", count.ToString());
+                path = GetURL($"{groupsBase}/{groupId}", queryParams);
+                var getRes = await GetItemAsync<Group>(path).ConfigureAwait(false);
+                if (!getRes.IsSuccess)
+                    return getRes;
+                if (getRes.Result.Members != null)
+                    group.Members.AddRange(getRes.Result.Members);
+            }
+            return initialGetRes;
+        }
+
+        /// <summary>
+        /// Update the group details, by ID.
+        /// Specify the group ID in the groupId parameter in the URI.
+        /// </summary>
+        /// <param name="groupId">A unique identifier for the group.</param>
+        /// <param name="displayName"></param>
+        /// <param name="description"></param>
+        /// <param name="members"></param>
+        /// <returns></returns>
         public async Task<SparkApiConnectorApiOperationResult<Group>> UpdateGroupsOverviewAsync(string groupId, string displayName = null, string description = null, List<PathMemberWithOperation> members = null)
         {
             var queryParams = new Dictionary<string, string>();

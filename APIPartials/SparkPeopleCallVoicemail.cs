4e834cca65788c7f62b53eb53e914436e31a457e
Michael Aebi <michael.aebi@sunrise.net>
2023-06-07 15:22:33 +0200

    Description added to the read method

--- APIPartials/SparkPeopleCallVoicemail.cs
+++ APIPartials/SparkPeopleCallVoicemail.cs
@@ -1,6 +1,14 @@
+using Newtonsoft.Json;
+
 using SparkDotNet.ExceptionHandling;
 using SparkDotNet.Models;
+
+using System;
+using SparkDotNet.Models.PersonCallVoicemail;
+
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime;
 using System.Threading.Tasks;
 
 namespace SparkDotNet
@@ -9,7 +17,13 @@ namespace SparkDotNet
     {
         private static string CallVoicemailBaseUrl { get; } = "/v1/people/{0}/features/voicemail";
 
-
+        /// <summary>
+        /// Read Voicemail Settings for a Person
+        /// Retrieve a person's Voicemail settings.
+        /// The voicemail feature transfers callers to voicemail based on your settings.You can then retrieve voice messages via Voicemail.Voicemail audio is sent in Waveform Audio File Format, .wav, format
+        /// Optionally, notifications can be sent to a mobile phone via text or email. These notifications will not include the voicemail files.
+        /// This API requires a full, user, or read-only administrator auth token with a scope of spark-admin:people_read or a user auth token with spark:people_read scope can be used by a person to read their settings.
+        /// </summary>
         public async Task<SparkApiConnectorApiOperationResult<PersonCallVoicemailSetting>> GetPersonVoicemailSettingAsync(string personId, string orgId = null)
         {
             return await GetPersonSettingAsync<PersonCallVoicemailSetting>(CallVoicemailBaseUrl, personId, orgId).ConfigureAwait(false);
